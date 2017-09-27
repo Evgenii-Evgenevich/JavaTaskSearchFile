76dbba49a154b7ce408d84b79b579499d18fa308
Evgenii Evgenievich <eb_genius@mail.ru>
2017-09-27 14:18:29 +0300

    throw

--- source/Main.java
+++ source/Main.java
@@ -2,18 +2,25 @@
  * Created by EE on 26.09.2017.
  */
 
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.*;
 import java.io.*;
 import java.util.stream.*;
 import java.util.zip.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    private static String zipName = null;
+
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
         System.out.println("Search for");
 
         Scanner scanner = new Scanner(System.in);
 
-        String zipName = scanner.nextLine();
+        zipName = scanner.nextLine();
 
         if (true) {
             ZipInputStream zipStream = new ZipInputStream(new FileInputStream(zipName));
@@ -21,8 +28,10 @@ public class Main {
             ZipEntry entry = null;
 
             while ((entry = zipStream.getNextEntry()) != null) {
-                Main.readEntry(entry);
+                Main.readEntry(entry, zipStream);
             }
+
+            zipStream.close();
         }
         else {
             ZipFile zipFile = new ZipFile(zipName);
@@ -32,13 +41,27 @@ public class Main {
             Iterator<? extends ZipEntry> iterator = entries.iterator();
 
             while (iterator.hasNext()) {
-                Main.readEntry(iterator.next());
+                ZipEntry entry = iterator.next();
+
+                InputStream in = zipFile.getInputStream(entry);
+
+                Main.readEntry(entry, in);
             }
+
+            zipFile.close();
         }
     }
 
+    public static void readEntry(ZipEntry entry, InputStream in) throws IOException, ParserConfigurationException, SAXException {
+        boolean isxml = entry.getName().endsWith(".xml");
 
-    public static void readEntry(ZipEntry entry) {
+        DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
 
+        DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+
+        Document document = docBuilder.parse(in);
+
+        //...
+        //...
     }
 }
